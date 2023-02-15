3998c2e48a968bc2078693a36ddc17f9859a0108
Fahri Hanif <fahrihanif03@gmail.com>
2023-02-15 10:41:27 +0700

    Seperated intreraction with database and ui

---
+++ ConnectionMCC75/Command/RegionCommands.cs
@@ -0,0 +1,94 @@
+﻿using ConnectionMCC75.Models;
+using System.Data.SqlClient;
+
+namespace ConnectionMCC75.Command;
+public class RegionCommands
+{
+    SqlConnection sqlConnection;
+    string connectionString = "Data Source=CAMOUFLY;Initial Catalog=db_hr;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+
+    public int Insert(Region entity)
+    {
+        int result = 0;
+        using (sqlConnection = new SqlConnection(connectionString))
+        {
+            sqlConnection.Open();
+            SqlTransaction sqlTransaction = sqlConnection.BeginTransaction();
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();
+            sqlCommand.Transaction = sqlTransaction;
+
+            try
+            {
+                sqlCommand.CommandText = "INSERT INTO tb_m_regions VALUES (@name);";
+
+                // Parameter Name
+                SqlParameter pName = new SqlParameter();
+                pName.ParameterName = "@name";
+                pName.SqlDbType = System.Data.SqlDbType.VarChar;
+                pName.Value = entity.Name;
+                sqlCommand.Parameters.Add(pName);
+
+                // Untuk menjalankan perintah transaksi
+                result = sqlCommand.ExecuteNonQuery();
+                sqlTransaction.Commit();
+                sqlConnection.Close();
+
+                return result;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                try
+                {
+                    sqlTransaction.Rollback();
+                }
+                catch (Exception exRollback)
+                {
+                    Console.WriteLine(exRollback.Message);
+                }
+            }
+
+            return result;
+        }
+    }
+
+    public List<Region> GetAll()
+    {
+        List<Region> listRegion = new List<Region>();
+
+        sqlConnection = new SqlConnection(connectionString);
+
+        // Membuat instance SqlCommand untuk mendifinisikan sebuah query & connection
+        SqlCommand sqlCommand = new SqlCommand();
+        sqlCommand.Connection = sqlConnection;
+        sqlCommand.CommandText = "SELECT * FROM tb_m_regions;";
+
+        // membuka koneksi
+        sqlConnection.Open();
+
+        using (SqlDataReader sqlDataReader = sqlCommand.ExecuteReader())
+        {
+            // Mengecek apakah ada data atau tidak
+            if (sqlDataReader.HasRows)
+            {
+                //jika ada, maka tampilkan datanya
+                while (sqlDataReader.Read())
+                {
+                    Console.WriteLine("Id : " + sqlDataReader[0]);
+                    Console.WriteLine("Name : " + sqlDataReader[1]);
+
+                    Region region = new Region();
+                    region.Id = Convert.ToInt16(sqlDataReader[0]);
+                    region.Name = sqlDataReader[1].ToString();
+
+                    listRegion.Add(region);
+                }
+            }
+            sqlDataReader.Close();
+        }
+        sqlConnection.Close();
+
+        return listRegion;
+    }
+
+}
--- ConnectionMCC75/Program.cs
+++ ConnectionMCC75/Program.cs
@@ -1,4 +1,5 @@
-﻿using ConnectionMCC75.Models;
+﻿using ConnectionMCC75.Command;
+using ConnectionMCC75.Models;
 using System.Data.SqlClient;
 
 namespace ConnectionMCC75;
@@ -10,100 +11,38 @@ public class Program
 
     public static void Main(string[] args)
     {
-        Program program = new Program();
+
         Region region = new Region();
-        region.Name = "cek 123";
+        region.Name = "cek 124";
+        region.Id = 1;
 
-        Console.WriteLine("saya Melakukan perubahan disini");
-        //program.GetRegions();
+        RegionCommands commands = new RegionCommands();
+        //int result = commands.Insert(region);
+        //if (result > 0)
+        //{
+        //    Console.WriteLine("Data Berhasil Disimpan");
+        //}
+        //else
+        //{
+        //    Console.WriteLine("Data Gagal Disimpan");
+        //}
 
-        //program.InsertRegion(region);
-        Console.WriteLine("test");
-        program.GetByIdRegion(5);
-    }
-
-    // Table Regions
-    // Create 
-    public void InsertRegion(Region entity)
-    {
-        using (sqlConnection = new SqlConnection(connectionString))
+        var results = commands.GetAll();
+        if (results == null)
         {
-            sqlConnection.Open();
-            SqlTransaction sqlTransaction = sqlConnection.BeginTransaction();
-            SqlCommand sqlCommand = sqlConnection.CreateCommand();
-            sqlCommand.Transaction = sqlTransaction;
-
-            try
+            Console.WriteLine("Data tidak ditemukan");
+        }
+        else
+        {
+            foreach (var item in results)
             {
-                sqlCommand.CommandText = "INSERT INTO tb_m_regions VALUES (@name);";
-                // INSERT INTO tb_m_regions VALUES ('entity.Name')
-
-                // Parameter Name
-                SqlParameter pName = new SqlParameter();
-                pName.ParameterName = "@name";
-                pName.SqlDbType = System.Data.SqlDbType.VarChar;
-                pName.Value = entity.Name;
-                sqlCommand.Parameters.Add(pName);
-
-                // Untuk menjalankan perintah transaksi
-                sqlCommand.ExecuteNonQuery();
-                sqlTransaction.Commit();
-
-                Console.WriteLine("Data Berhasil Di Masukan");
-
-                sqlConnection.Close();
+                Console.WriteLine("Id : " + item.Id);
+                Console.WriteLine("Name : " + item.Name);
             }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-                try
-                {
-                    sqlTransaction.Rollback();
-                }
-                catch (Exception exRollback)
-                {
-                    Console.WriteLine(exRollback.Message);
-                }
-            }
-
         }
     }
 
-    // Read / View
-    public void GetRegions()
-    {
-        sqlConnection = new SqlConnection(connectionString);
-
-        // Membuat instance SqlCommand untuk mendifinisikan sebuah query & connection
-        SqlCommand sqlCommand = new SqlCommand();
-        sqlCommand.Connection = sqlConnection;
-        sqlCommand.CommandText = "SELECT * FROM tb_m_regions;";
-
-        // membuka koneksi
-        sqlConnection.Open();
-
-        using (SqlDataReader sqlDataReader = sqlCommand.ExecuteReader())
-        {
-            // Mengecek apakah ada data atau tidak
-            if (sqlDataReader.HasRows)
-            {
-                //jika ada, maka tampilkan datanya
-                while (sqlDataReader.Read())
-                {
-                    Console.WriteLine("Id : " + sqlDataReader[0]);
-                    Console.WriteLine("Name : " + sqlDataReader[1]);
-                }
-            }
-            else
-            {
-                Console.WriteLine("Data Kosong");
-            }
-            sqlDataReader.Close();
-        }
-        sqlConnection.Close();
-    }
-
-    // Read by Id
+    // Region
     public void GetByIdRegion(int id)
     {
         sqlConnection = new SqlConnection(connectionString);
@@ -151,5 +90,103 @@ public class Program
     }
 
     // Update
+    // Int
+    public void UpdateRegion(Region entity)
+    {
+        using (sqlConnection = new SqlConnection(connectionString))
+        {
+            sqlConnection.Open();
+            SqlTransaction sqlTransaction = sqlConnection.BeginTransaction();
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();
+            sqlCommand.Transaction = sqlTransaction;
+
+            try
+            {
+                sqlCommand.CommandText = "UPDATE tb_m_regions SET name = @name WHERE id = @id;";
+
+                // Parameter Name
+                SqlParameter pName = new SqlParameter
+                {
+                    ParameterName = "@name",
+                    SqlDbType = System.Data.SqlDbType.VarChar,
+                    Value = entity.Name
+                };
+                sqlCommand.Parameters.Add(pName);
+
+                // Parameter Id
+                SqlParameter pId = new SqlParameter
+                {
+                    ParameterName = "@id",
+                    SqlDbType = System.Data.SqlDbType.Int,
+                    Value = entity.Id
+                };
+                sqlCommand.Parameters.Add(pId);
+
+                // Untuk menjalankan perintah transaksi
+                sqlCommand.ExecuteNonQuery();
+                sqlTransaction.Commit();
+
+                Console.WriteLine("Data Berhasil Di Perbaharui");
+
+                sqlConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                try
+                {
+                    sqlTransaction.Rollback();
+                }
+                catch (Exception exRollback)
+                {
+                    Console.WriteLine(exRollback.Message);
+                }
+            }
+        }
+    }
+
     // Delete
+    // Int
+    public void DeleteRegion(int id)
+    {
+        using (sqlConnection = new SqlConnection(connectionString))
+        {
+            sqlConnection.Open();
+            SqlTransaction sqlTransaction = sqlConnection.BeginTransaction();
+            SqlCommand sqlCommand = sqlConnection.CreateCommand();
+            sqlCommand.Transaction = sqlTransaction;
+
+            try
+            {
+                sqlCommand.CommandText = "DELETE FROM tb_m_regions WHERE id = @id;";
+
+                // Parameter Id
+                SqlParameter pId = new SqlParameter();
+                pId.ParameterName = "@id";
+                pId.SqlDbType = System.Data.SqlDbType.Int;
+                pId.Value = id;
+                sqlCommand.Parameters.Add(pId);
+
+                // Untuk menjalankan perintah transaksi
+                sqlCommand.ExecuteNonQuery();
+                sqlTransaction.Commit();
+
+                Console.WriteLine("Data Berhasil Di Hapus");
+
+                sqlConnection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                try
+                {
+                    sqlTransaction.Rollback();
+                }
+                catch (Exception exRollback)
+                {
+                    Console.WriteLine(exRollback.Message);
+                }
+            }
+        }
+    }
 }
